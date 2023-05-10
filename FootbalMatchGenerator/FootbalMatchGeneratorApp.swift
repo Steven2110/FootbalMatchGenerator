0a21c3ce6c77617b5ca840f0734dd65451859cd1
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-05-11 01:58:48 +0700

    Initial Commit

---
+++ FootbalMatchGenerator/FootbalMatchGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootbalMatchGeneratorApp.swift
+//  FootbalMatchGenerator
+//
+//  Created by Steven Wijaya on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FootbalMatchGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
